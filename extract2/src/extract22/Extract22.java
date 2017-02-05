2517ce4e61ae5a90c702ea2aff84413ac19ef92a
ruizhewu <ruizhewu@gmail.com>
2017-02-04 21:54:07 -0800

    Extract22.java

--- extract2/src/extract22/Extract22.java
+++ extract2/src/extract22/Extract22.java
@@ -16,13 +16,15 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import java.io.InputStreamReader;  
+import java.util.Iterator;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;  
 import java.net.URL;  
 import java.util.ArrayList;  
@@ -39,19 +41,26 @@ import org.jsoup.select.Elements;
 
 import org.jsoup.helper.Validate;
 
-
+class ExtractDoc{
+	String author;
+	String doc;
+	
+}
 
 public class Extract22 {
-	public String getpage(String URL){
+	ArrayList StructDoc;  
+	public String getpage(String URL) throws IOException{
 		String pattern = "/p/\\d+";
 		//中文
 		Pattern r = Pattern.compile(pattern);
 		//Matcher m = r.matcher(pageurl);
 		String filename="C:\\Users\\ruizhewu\\heichuizi\\test.html";
+		String savePath="C:\\Users\\ruizhewu\\heichuizi\\";
 		File file = new File(filename);
 		String tempString = null;
 		BufferedReader reader = null;
 		String content=null;
+		StructDoc= new ArrayList(); 
 		try {
 			System.out.println("*********************");
 			reader = new BufferedReader(new FileReader(file));
@@ -78,7 +87,7 @@ public class Extract22 {
 		Elements fool =doc.getElementsByTag("h3"); //Get the tile of Each post thread
 		Elements fool2 =doc.getElementsByClass("d_post_content j_d_post_content "); //Get the tile of Each post thread
 		Elements fool3 =doc.getElementsByClass("louzhubiaoshi  j_louzhubiaoshi"); //Get the tile of Each post thread
-		Elements fool4 =doc.getElementsByClass("p_author_name j_user_card"); //Get the tile of Each post thread
+		Elements fool4 =doc.getElementsByTag("a"); //Get the tile of Each post thread
 		//<div class="louzhubiaoshi  j_louzhubiaoshi" author="xl19931215">
 		//class="d_post_content j_d_post_content
 		//class="d_name"
@@ -87,22 +96,58 @@ public class Extract22 {
 		 }
 		 System.out.println("*1*******************");
 		 for (Element link : fool2) { 
+			 ExtractDoc tmp=new ExtractDoc();
+			 tmp.doc=link.text();
+			 StructDoc.add(tmp);
 			 System.out.println(link.text());
 		 }
-		 System.out.println("*2*******************");
-		 for (Element link : fool3) { 
-			 System.out.println(link.attr("author").toString());
-		 }
 		 System.out.println("*3*******************");
+		 System.out.println("Size="+fool4.size());
+		 int tmpCount=0;
+		 Iterator<ExtractDoc> iterator = StructDoc.iterator();
 		 for (Element link : fool4) { 
-			 //System.out.println(link.);
+			 if(link.attr("data-field").isEmpty()==false){
+				 String tmpId=link.attr("data-field");
+				 tmpId=tmpId.substring(0, 5);
+				 if(tmpId.equals("{\"un\""))
+				 {
+					 ExtractDoc tmp =iterator.next();
+					 String tmpAuthor=link.attr("data-field");
+					 int index=tmpAuthor.indexOf("\"}");
+					 tmp.author=tmpAuthor.substring(7, index);
+					 System.out.println(tmp.author);
+					 tmpCount++;
+				 }
+			 }
 		 }
+		 System.out.println("tmpCount="+tmpCount);
+		 Iterator<ExtractDoc> iterator1 = StructDoc.iterator();
+		 String fileName=savePath+URL;
+		 //FileWriter writer = null;
+		//try {
+		//	writer = new FileWriter(fileName);
+		//} catch (IOException e) {
+			// TODO Auto-generated catch block
+		//	e.printStackTrace();
+		//}
+		OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(fileName),"UTF-8");
+		 while(iterator1.hasNext()){
+			 ExtractDoc tmp=iterator1.next();
+			 System.out.println(tmp.author+"    "+tmp.doc);
+			 out.write("\n\t"+tmp.author+"    "+tmp.doc);
+		 }
+		 out.close();
 		//doc.getElementsByTag();
 		return content;
 	}
 	public static void main(String[] ags){
 		Extract22 T=new Extract22();
-		T.getpage("123");
+		try {
+			T.getpage("123");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
